9ae196f4440ce4b90d47b06023574eb283cb7534
santiagorejas <santiago.orejasrg@gmail.com>
2023-03-23 15:25:53 -0300

    last edited field added to note response

--- backend/src/main/java/com/example/backend/model/response/NoteResponse.java
+++ backend/src/main/java/com/example/backend/model/response/NoteResponse.java
@@ -3,12 +3,14 @@ package com.example.backend.model.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class NoteResponse {
 
     private String noteId;
     private String title;
-    private String content;
+    private Date lastEdited;
 
 }
