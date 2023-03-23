ab947fba5208c84b9174156996437e9c99642c66
santiagorejas <santiago.orejasrg@gmail.com>
2023-03-23 16:04:48 -0300

    buttons added to notes list

--- frontend/src/components/NotesList/NotesList.jsx
+++ frontend/src/components/NotesList/NotesList.jsx
@@ -6,29 +6,40 @@ import classes from "./NotesList.module.css";
 
 const NotesList = () => {
   const [notes, setNotes] = useState([]);
+  const [showArchived, setShowArchived] = useState(false);
+  const [page, setPage] = useState(1);
 
   const { sendRequest, isLoading, error, clearError } = useHttp();
 
   useEffect(() => {
     const fetchNotes = async () => {
-      console.log(`${import.meta.env.VITE_API_URL}/api/v1/notes`);
       const data = await sendRequest(
-        `${import.meta.env.VITE_API_URL}/api/v1/notes`
+        `${import.meta.env.VITE_API_URL}/api/v1/notes?page=${
+          page - 1
+        }&size=10&archived=${showArchived}`
       );
 
       setNotes(data.content);
     };
 
     fetchNotes();
-  }, []);
+  }, [showArchived, page]);
 
   return (
     <div className={classes["notes-list"]}>
-      <div>
-        <h1>NotesList</h1>
-        <div>
+      <div className={classes["notes-list__header"]}>
+        <h1 className={classes["notes-list__title"]}>NotesList</h1>
+        <div className={classes["notes-list__buttons"]}>
           <Button>Create Note</Button>
           <Button>Create Category</Button>
+          <button
+            className={classes["notes-list__archived-btn"]}
+            onClick={() => {
+              setShowArchived((pre) => !pre);
+            }}
+          >
+            {showArchived ? "Show unarchived" : "Show archived"}
+          </button>
         </div>
       </div>
       <ul className={classes["notes-container"]}>
