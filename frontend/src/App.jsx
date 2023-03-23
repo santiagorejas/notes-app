d9a7331f90f843134cdfc18a704a575fe61cbc86
santiagorejas <santiago.orejasrg@gmail.com>
2023-03-23 15:04:16 -0300

    cors config added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/api/v1/notes");
+      const data = await response.json();
+      console.log(data);
+    };
+
+    fetchData();
+  });
 
   return (
     <div className="App">
